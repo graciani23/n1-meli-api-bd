4a9d47e9437ff5d8abbff30bd784cde9d1a71eaa
graciani23 <gracianigss@gmail.com>
2019-11-13 22:10:05 -0300

    criação de rotas getAge e getBooks

--- src/controllers/alunasController.js
+++ src/controllers/alunasController.js
@@ -12,35 +12,40 @@ exports.get = (req, res) => {
 }
 
 
+//Código da prof Marília
 exports.getById = (req, res) => {
-  Alunas.findById({ _id: req.params.id }, (function(err, aluna) {
-    if (err) res.status(500).send(err)
-    res.status(200).send(aluna)
+  const alunaId = req.params.id
+  Alunas.findById(alunaId, (function (err, aluna) {
+    if (err) return res.status(500).send(err);
+
+    if (!aluna) {
+      return res.status(200).send({ message: `Infelizmente não localizamos a aluna de id: ${alunaId}` })
+    }
+    res.status(200).send(aluna);
   }))
 }
 
+// exports.getById = (req, res) => {
+//   Alunas.findById({ _id: req.params.id }, (function(err, aluna) {
+//     if (err) res.status(500).send(err)
+//     res.status(200).send(aluna)
+//   }))
+// }
+
+
+
 
 exports.getBooks = (req, res) => {
-  const id = req.params.id
-  const aluna = alunas.find(aluna => aluna.id == id)
-  if (!aluna) {
-    res.send("Nao encontrei essa garota")
-  }
-  const livrosAluna = aluna.livros
-  const livrosLidos = livrosAluna.filter(livro => livro.leu == "true")
-  const tituloLivros = livrosLidos.map(livro => livro.titulo)
-  res.send(tituloLivros)
+  const alunaId = req.params.id
+  Alunas.findById(alunaId, (function (err, aluna) {
+    if (err) return res.status(500).send(err);
+    const livroAluna = aluna.livros
+    const livroLeu = livroAluna.filter(livro => livro.leu == "true")
+    const livroLido = livroLeu.map(livro => livro.titulo)
+    res.status(200).send(livroLido)
+  }))
 }
 
-// exports.getSp = (req, res) => {
-//   const nasceuSp = alunas.filter(aluna => {
-//     console.log(aluna)
-//     return aluna.nasceuEmSp == "true"
-//   })
-//   const meninasSp = nasceuSp.map(aluna => aluna.nome)
-
-//   res.status(200).send(meninasSp)
-// }
 
 exports.getSp = (req, res) => {
   Alunas.find({ "nasceuEmSp": "true" }, (function (err, alunas) {
@@ -50,19 +55,31 @@ exports.getSp = (req, res) => {
   }))
 }
 
-
-
 exports.getAge = (req, res) => {
   const id = req.params.id
-  const aluna = alunas.find(item => item.id == id)
-  const dataNasc = aluna.dateOfBirth
-  const arrData = dataNasc.split("/")
-  const dia = arrData[0]
-  const mes = arrData[1]
-  const ano = arrData[2]
-  const idade = calcularIdade(ano, mes, dia)
-  res.status(200).send({ idade })
+  Alunas.findById(id, (function (err, aluna) {
+
+    if (err) return res.status(500).send(err)
+
+    const dataNasc = aluna.dateOfBirth
+    const arrData = dataNasc.split("/")
+    const dia = arrData[0]
+    const mes = arrData[1]
+    const ano = arrData[2]
+    const idade = calcularIdade(ano, mes, dia)
+    res.status(200).send({ idade })
+  }))
 }
+
+//   const aluna = alunas.find(item => item.id == id)
+//   const dataNasc = aluna.dateOfBirth
+//   const arrData = dataNasc.split("/")
+//   const dia = arrData[0]
+//   const mes = arrData[1]
+//   const ano = arrData[2]
+//   const idade = calcularIdade(ano, mes, dia)
+//   res.status(200).send({ idade })
+
 
 function calcularIdade(anoDeNasc, mesDeNasc, diaDeNasc) {
   const now = new Date()
